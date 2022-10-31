d1c636e096d9bc925253dbbc6262f260e99426b4
Ivan <ivanscopel@gmail.com>
2022-10-31 19:31:21 -0300

    init push (real)

--- src/main/java/models/Organizacion_Donante.java
+++ src/main/java/models/Organizacion_Donante.java
@@ -9,4 +9,11 @@ public class Organizacion_Donante {
 	private String cuil;
 	private String direccion;
 	private List<Contacto> contactos;
+
+	public Organizacion_Donante(String razon_social, String cuil, String direccion, List<Contacto> contactos {
+		this.razon_social = razon_social;
+		this.cuil = cuil;
+		this.direccion = direccion;
+		this.contactos = contactos
+	}
 }
