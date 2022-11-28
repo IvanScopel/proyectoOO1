2f534720793bce332c798316926aff69d2269409
Leandro Baez <devbaezleandro@gmail.com>
2022-11-28 12:49:50 -0300

    se agrego documentacion

--- src/main/java/models/Voluntario.java
+++ src/main/java/models/Voluntario.java
@@ -22,9 +22,8 @@ public class Voluntario {
 	public Voluntario altaVoluntario(String nombre, String dni, String email, String whatsapp, String direccion_estable,Vehiculo vehiculo) {
 		return new Voluntario(nombre,dni,email,whatsapp,direccion_estable,vehiculo);
 	}
-	
+	//retorna si posee vehiculo en caso contrario retorna null
 	public Vehiculo getVehiculo(){
-		
 		if(vehiculo!=null) return vehiculo;
 		return null;
 	}
