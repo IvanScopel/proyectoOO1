a689bf359745cc9297fdb23b1e5021e584acf3db
Leandro Baez <leandro.baez@cajapolicia.gba>
2022-12-02 12:11:05 -0300

    se agregaron jtest

---
+++ src/test/java/models/BeneficiarioTest.java
@@ -0,0 +1,14 @@
+package models;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BeneficiarioTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/models/DescripcionGeneralTest.java
@@ -0,0 +1,14 @@
+package models;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DescripcionGeneralTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
