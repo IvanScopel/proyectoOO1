cd093b107f5befd85e86bb169a7bdef8b38d4bf4
Leandro Baez <devbaezleandro@gmail.com>
2022-12-11 14:40:56 -0300

    a

---
+++ src/test/java/models/ResumenEnvioTest.java
@@ -0,0 +1,14 @@
+package models;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ResumenEnvioTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
